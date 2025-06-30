da7121501009ddd19408d278ac3778b332b88a7e
DimBurCat <dmitriburenin3@gmail.com>
2025-06-30 18:16:37 +0300

    Add files via upload

--- JS/form.js
+++ JS/form.js
@@ -1,5 +1,56 @@
-const btnForm = document.getElementById("btnForm")
+const btnForm = document.getElementById("btnForm");
+const telNum = document.getElementById("input-telNum");
+const username = document.getElementById("input-name");
+const email = document.getElementById("input-email");
+const form = document.getElementById("form");
+let numVal = false;
+let nameVal = false;
+let emVal = false;
+
+username.addEventListener("input", (event) => {
+  const regex = /^[a-zA-ZА-Яа-я][a-zA-ZА-Яа-я0-9-_\.]{1,20}$/;
+  nameVal = regex.test(event.target.value);
+  if (nameVal) {
+    event.target.style.background = "var(--white-color)";
+    event.target.style.color = "var(--dark-puple-color)";
+  } else {
+    event.target.style.background = "var(--bloodly-red-color)";
+    event.target.style.color = "var(--white-color)";
+  }
+});
+
+telNum.addEventListener("input", (event) => {
+  const regex = /^((\+7|7|8)+([0-9]){10})$/;
+  numVal = regex.test(event.target.value);
+  if (numVal) {
+    event.target.style.background = "var(--white-color)";
+    event.target.style.color = "var(--dark-puple-color)";
+  } else {
+    event.target.style.background = "var(--bloodly-red-color)";
+    event.target.style.color = "var(--white-color)";
+  }
+});
+
+email.addEventListener("input", (event) => {
+  const regex = /^[a-zA-ZА-Яа-я0-9._%+-]+@[a-zA-ZА-Яа-я0-9.-]+\.[a-zA-ZА-Яа-я]{2,}$/;
+  emVal = regex.test(event.target.value);
+  if (emVal) {
+    event.target.style.background = "var(--white-color)";
+    event.target.style.color = "var(--dark-puple-color)";
+  } else {
+    event.target.style.background = "var(--bloodly-red-color)";
+    event.target.style.color = "var(--white-color)";
+  }
+});
+
+form.addEventListener("change", () => {
+  if (emVal && numVal && nameVal) {
+    btnForm.disabled = false;
+  } else {
+    btnForm.disabled = true;
+  }
+});
 
 btnForm.addEventListener('click', () => {
-    alert('Ваша заявка отправлена! Ждите обратного сообщения на вашу почту.')
-})
+    alert("Ваша заявка успешно отправлена! С вами свяжутся в ближайшее время")
+});
--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,16 @@
 const buttonMenu = document.getElementById("buttonMenu")
 const NavigationMenu = document.getElementById("NavigationMenu")
 const buttonMenuClose = document.getElementById("buttonMenuClose")
+const btnOpenWindow_1 = document.getElementById("1");
+const btnOpenWindow_2 = document.getElementById("2");
+const btnOpenWindow_3 = document.getElementById("3");
+const btnOpenWindow_4 = document.getElementById("4");
+const btnOpenWindow_5 = document.getElementById("5");
+let modalWindowImg = document.getElementById("modalWindowImg");
+let modalWindowNickname = document.getElementById("modalWindowNickname");
+let modalWindowdecsription = document.getElementById("modalWindowdecsription");
+const tranModalCloseBtn = document.getElementById("tranModalCloseBtn");
+const modalWindow = document.getElementById("modalWindow"); ;
 
 buttonMenu.addEventListener('click', () => {  
     NavigationMenu.style.transform = "translateX" + "(" + "0" + ")";
@@ -10,3 +20,39 @@ buttonMenuClose.addEventListener('click', () => {
     NavigationMenu.style.transform = "translateX" + "(" + "100" + ")";
     NavigationMenu.style.display = "none";
 })
+
+tranModalCloseBtn.addEventListener('click', () => {
+    modalWindow.close();
+});
+
+btnOpenWindow_1.addEventListener("click", () => {
+    modalWindowImg.src = "./IMG/traner_1.png";
+    modalWindowNickname.textContent = "Ирина Лайм";
+    modalWindowdecsription.textContent = "Ноябрь 1993 — Январь 2000";
+    modalWindow.showModal();
+});
+btnOpenWindow_2.addEventListener("click", () => {
+    modalWindowImg.src = "./IMG/traner_2.png";
+    modalWindowNickname.textContent = "Марина Орлова";
+    modalWindowdecsription.textContent = "Сентябрь 1995 — Июнь 2000";
+    modalWindow.showModal();
+});
+btnOpenWindow_3.addEventListener("click", () => {
+    modalWindowImg.src = "./IMG/traner_3.png";
+    modalWindowNickname.textContent = "Максим Петров";
+    modalWindowdecsription.textContent = "Октябрь 1994 — Июль 2001";
+    modalWindow.showModal();   
+});
+btnOpenWindow_4.addEventListener("click", () => {
+    modalWindowImg.src = "./IMG/traner_4.png";
+    modalWindowNickname.textContent = "Константин Назаров";
+    modalWindowdecsription.textContent = "Сентябрь 1996 — Август 2002";
+    modalWindow.showModal();
+});
+btnOpenWindow_5.addEventListener("click", () => {
+    modalWindowImg.src = "./IMG/traner_5.png";
+    modalWindowNickname.textContent = "Лиза Весенняя";
+    modalWindowdecsription.textContent = "Ноябрь 1998 — Июнь 2004";
+    modalWindow.showModal();
+});
+
